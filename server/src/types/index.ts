49a5c494f4f98ef103b34c5c10cae0cb11b5191e
Santiago Ayala <santitomayala07@gmail.com>
2026-04-17 22:57:09 -0300

    Se creó el archivo client.ts con la instancia de PrismaClient y se creó el archivo index.ts con las interfaces AuthPayload y AuthRequest.

---
+++ server/src/types/index.ts
@@ -0,0 +1,11 @@
+import { Request } from "express";
+
+export interface AuthPayload {
+  id: number;
+  email: string;
+  rol: "OPERADOR" | "ADMINISTRADOR";
+}
+
+export interface AuthRequest extends Request {
+  user?: AuthPayload;
+}
